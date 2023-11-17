136f4ef2f6ac3c0675ee1f8f3c815e2aebd14709
syjJustin <syjnl1250923797@gmail.com>
2023-11-16 16:18:11 -0800

    change the code and csv

--- starbucks_news.py
+++ starbucks_news.py
@@ -1,30 +1,24 @@
-from http import client
-from urllib import parse
-import json
-import pandas as pd
-
 class get_news:
     
-    def __init__(self, company_name, end_date, iteration, api_token, conn):
+    def __init__(self, company_name = None, end_date = None, pages = None, 
+                 api_token = None, conn = None):
         
         # company_name is the company we focus on
         # end_date is the latest date of news we consider
-        # iteration: we cannot retrieve more than 20000 articles at a time
     
         self.company_name = company_name
         self.end_date = end_date
-        self.iteration = iteration
+        self.pages = pages
         self.api_token = api_token
         self.conn = conn
         
     def retrieve_raw_data(self):
         news_list = []
         
-        # we cannot retrieve more than 20000 articles
-        # which means we can only have 800 pages, each page 25 articles.
-        for i in range(1, self.iteration + 1):
+        for i in tqdm(range(1, self.pages + 1)):
             params = parse.urlencode({"api_token": self.api_token,
                                              "search": self.company_name,
+                                             "search_fields": "description,title,keywords",
                                              "language": "en",
                                              "published_before": self.end_date,
                                              "page": i,
@@ -49,19 +43,81 @@ class get_news:
         
         return elements 
     
-
-if __name__ == "__main__":
-    api_token = "1XOrWumzd3Lw99zn156obKbYmWS9wtVw4FX4LiuS"
-    conn = client.HTTPSConnection('api.thenewsapi.com')
-    elements = ["title", "description", "keywords", 
-                "url", "published_at", "source", "categories"]
-    df = pd.DataFrame()
-    
-    starbucks = get_news("Starbucks", "2023-11-09", 800, api_token, conn)
-    raw_data = starbucks.retrieve_raw_data()
-    
-    for x in elements:
-        ele = starbucks.element(raw_data, x)
-        df[x] = ele
+    def clean(self, df):
+        df["date"] = df["published_at"].str[:10]
+        df["where"] = df["source"].str.split(".").str[:-1].str.join(" ")
+        df = df.sort_values(by = "date").reset_index()
+        df.drop(columns = ["published_at", "source", "index"], inplace = True)
         
-    df.to_csv("Starbucks_news.csv")
+        return df
+    
+    def understand_data(self, df):
+        '''
+        to run visualization, do the following:
+        object = get_news()
+        fig = object.understand_data(data)
+        fig
+        '''
+        yearly_info = df.groupby("date").apply(len).reset_index()
+        yearly_info = yearly_info.rename(columns = {0: "news_each_day"})
+        yearly_info["year"] = yearly_info["date"].str.split("-").str[0]
+        yearly_info["days"] = yearly_info.groupby("year")["year"].transform(len)
+        yearly_info["median"] = round(yearly_info.groupby("year")["news_each_day"].transform(np.median))
+    
+    
+        fig = px.scatter(yearly_info,
+                         x = "date",
+                         y = "news_each_day",
+                         color = "year",
+                         hover_name = "days",
+                         marginal_y = "box",
+                         title = "Starbucks",
+                         width = 800, 
+                         height = 450)
+        fig.update_layout(margin={"r":130, "t":30, "l":0, "b":0})
+    
+        return fig
+    
+
+# if __name__ == "__main__":
+#     import json
+#     import pandas as pd
+#     import numpy as np
+#     from http import client
+#     from urllib import parse
+#     from tqdm import tqdm
+#     from matplotlib import pyplot as plt
+#     from plotly import express as px
+    
+#     api_token = "1XOrWumzd3Lw99zn156obKbYmWS9wtVw4FX4LiuS"
+#     conn = client.HTTPSConnection('api.thenewsapi.com')
+#     elements = ["title", "description", "keywords", 
+#                 "url", "published_at", "source", "categories"]
+#     df = pd.DataFrame()
+
+#     # use page = 1 to get the total number of news first
+#     trial = get_news("Starbucks", "2023-11-15", 1, api_token, conn) 
+#     trial_data = trial.retrieve_raw_data()
+#     news_number = int(trial_data[0]["meta"]['found'])
+#     pages = news_number // 25
+#     if news_number % 25 != 0:
+#         pages += 1
+    
+#     starbucks = get_news("Starbucks", "2023-11-15", pages, api_token, conn)
+#     starbucks_data = starbucks.retrieve_raw_data()
+
+#     for x in tqdm(elements):
+#         ele = starbucks.element(starbucks_data, x)
+#         df[x] = ele
+    
+#     clean_df = starbucks.clean(df)
+#     starbucks.understand_data(clean_df)
+#     clean_df.to_csv("Starbucks_news.csv")
+
+#     # if you want to do a new company, copy the code above and change some parameters.
+    
+#     # if you want to simply visualize the data for starbucks, after import the data and class, do:
+#     df = pd.read_csv("Starbucks_news.csv")
+#     starbucks = get_news()
+#     starbucks.understand_data(df)
+    
