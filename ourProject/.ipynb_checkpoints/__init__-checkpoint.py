d55041ac073468d67687652414f5405583787efd
syjJustin <syjnl1250923797@gmail.com>
2023-12-15 12:24:32 -0800

    change

--- ourProject/.ipynb_checkpoints/__init__-checkpoint.py
+++ ourProject/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,4 +1,4 @@
-from ourProject import starbucks_news
-from ourProject import ModelPred
-from ourProject import stockprice
-from ourProject import TextDataPrep
+from ourProject.starbucks_news import get_news
+from ourProject.ModelPred import TrainModel, pred_scores
+from ourProject.stockprice import create_data, stock_price_change, stock_price_percentage, clean_data, add_features, pred_target, scalers, train_val_test, split_data, compile_train, plot_history, evaluate_model
+from ourProject.TextDataPrep import prepData, negative_aug, remove_punc, get_wordnet_pos, lemma, make_dataset
