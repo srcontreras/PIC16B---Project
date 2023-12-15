b13d41c3dd918047b2ebdb4acb42b25520f049e4
srcontreras <146390137+srcontreras@users.noreply.github.com>
2023-12-15 10:47:39 -0800

    updates

---
+++ ourProject/.ipynb_checkpoints/starbucks_news-checkpoint.py
@@ -0,0 +1,201 @@
+import json
+import pandas as pd
+import numpy as np
+from http import client
+from urllib import parse
+from matplotlib import pyplot as plt
+from plotly import express as px
+
+class get_news:
+    '''
+    ***** IMPORTANT NOTE *****
+    The News API used is not free. Anyone who runs our project shouldn't run the methods in this class,
+    except understand_data(df), which presents plots. Instead, we have created Starbucks_news.csv dataset, 
+    which contains all the textual information we need. 
+    Direcly reading from this .csv dataframe is recommended. 
+    **************************
+    
+    This class is for retrieving the news article of Starbucks from 2021-01-01 to 2023-11-14. A news API called
+    TheNewsAPI is used for data collection. Data cleaning and visualization are also implemented in this class.
+    As notified, the API is not free, so re-running is not recommended. To see how to retrieve data from this 
+    API, see the code in __main__() at the bottom.
+    
+    Attributes:
+    ----------
+    company_name: str, name of the company; we want our news article to focus on this company; default Starbucks;
+    end_date: str, the date of the latest news article that we incorporate; default 2023-11-14;
+    pages: int, the pages of news articles that are about to be retrieved; default None;
+    api_token: str, the api key for accessing the TheNewsAPI; default None;
+    conn: object, connector that allows us to access the API; default None.
+    
+    Methods:
+    -------
+    retrieve_raw_data(): collect all the pages that contain news article from 2021 up to 2023-11-14;
+    element(raw_data, objects): store the feature of each news article;
+    clean(df): clean the raw dataset;
+    understand_data(df): visualize the number of news articles each day.
+    '''
+    
+    def __init__(self, company_name = 'Starbucks', end_date = '2023-11-14', pages = None, 
+                 api_token = None, conn = None):
+        '''
+        @param company_name: str, we only focus on news articles of this company; default Starbucks
+        @param end_date: str, we only consider news articles up to this date; default 2023-11-14
+        @param pages: int, the number of pages retrieved from the API; default None
+        @param api_token: str, the api key for accessing the API; default None
+        @param conn: object, the connector that allows to access the API; default None
+        '''
+        
+        self.company_name = company_name
+        self.end_date = end_date
+        self.pages = pages
+        self.api_token = api_token
+        self.conn = conn
+        
+    def retrieve_raw_data(self):
+        '''
+        This method helps us collect news articles of Starbucks from the TheNewsAPI. We append each page
+        from the API to a list, and use it as our raw data for future processes. No parameter is required:
+        all necessary ones are defined in the constructor.
+        
+        @rvalue: list, the return value is a list that contains all pages of news articles of Starbucks. 
+        '''
+        
+        news_list = [] # place to store raw data
+        
+        # append each page to the list
+        for i in range(1, self.pages + 1):
+            
+            #general settings of accessing the API
+            params = parse.urlencode({"api_token": self.api_token,
+                                             "search": self.company_name,
+                                             "search_fields": "description,title,keywords",
+                                             "language": "en",
+                                             "published_before": self.end_date,
+                                             "page": i,
+                                             "limit": 25})
+            self.conn.request("GET", "/v1/news/all?{}".format(params)) # connect to the API
+            response = self.conn.getresponse()
+            data = response.read()
+            info = data.decode("utf-8") # information of each page of Starbucks news article
+            json_info = json.loads(info) # the info is json format, we transform it into Python dict
+            
+            news_list.append(json_info)
+        
+        return news_list
+    
+    def element(self, raw_data, feature):
+        '''
+        This method collects details (features) of the news article from the raw data we get from 
+        retrieve_raw_data() method. To be specific, we consider 7 features: 
+        "title", "description", "keywords", "url", "published_at", "source", and "categories". 
+        This method focuses on one of them and find the corresponding feature in the raw data, 
+        extract it and save it into a list. To cover all the 7 features, we will use a for loop in the main scope.
+        
+        @param raw_data: list, the raw data that contains all pages of the news articles;
+        @feature: str, one of the 7 features listed above.
+        
+        @rvalue: list, the return value is a list that contains the corresponding feature of each news article
+                 in the raw data.
+        '''
+        
+        elements = [] # place to store the feature of all news articles
+        
+        # loop over each page
+        for i in raw_data:
+            each_data = i["data"] # the news articles of one page are stored as value of key 'data'
+            for j in each_data: # loop through each news articles
+                elements.append(j[feature]) # extract the corresponding feature
+        
+        return elements 
+    
+    def clean(self, df):
+        '''
+        This method cleans the dataframe of Starbucks news articles, such as truncate the publish date of 
+        each article, clean the source, and sort the dataframe based on publish date, from oldest to latest.
+        
+        @param df: dataframe, each column of the dataframe is one feature of the news article, which is returned
+                   by the element(raw_data, feature) method. 
+        
+        @rvalue: dataframe, which is the cleaned version of the original df.
+        '''
+        
+        df["date"] = df["published_at"].str[:10] # Year-Month-Date
+        df["where"] = df["source"].str.split(".").str[:-1].str.join(" ") # clean source
+        df = df.sort_values(by = "date").reset_index() # sort from oldest to latest news articles
+        df.drop(columns = ["published_at", "source", "index"], inplace = True)
+        
+        return df
+    
+    def understand_data(self, df):
+        '''
+        This method visualizes the number of news articles on each day, along with the median of number of each year.
+        This can help understand the distribution of our data, which can be used to make inference of the 
+        performance of prediction later. 
+        
+        @param df: dataframe, the cleaned version of dataset, returned by clean(df).
+        
+        No return value, but show the plot.
+        '''
+        
+        yearly_info = df.groupby("date").apply(len).reset_index() # number of news on each day
+        yearly_info = yearly_info.rename(columns = {0: "news_each_day"})
+        yearly_info["year"] = yearly_info["date"].str.split("-").str[0] # get year
+        yearly_info["days"] = yearly_info.groupby("year")["year"].transform(len) 
+        yearly_info["median"] = round(yearly_info.groupby("year")["news_each_day"].transform(np.median))
+    
+        # plot figure
+        fig = px.scatter(yearly_info,
+                         x = "date",
+                         y = "news_each_day",
+                         color = "year",
+                         hover_name = "days",
+                         marginal_y = "box",
+                         title = "Starbucks",
+                         width = 800, 
+                         height = 450)
+        fig.update_layout(margin={"r":130, "t":30, "l":0, "b":0})
+    
+        fig.show()
+    
+
+if __name__ == "__main__":
+    
+    # settings of the TheNewsAPI
+    api_token = "1XOrWumzd3Lw99zn156obKbYmWS9wtVw4FX4LiuS"
+    conn = client.HTTPSConnection('api.thenewsapi.com')
+    elements = ["title", "description", "keywords", 
+                "url", "published_at", "source", "categories"] # columns of the dataset
+    df = pd.DataFrame()
+
+    # use page = 1 to get the total number of news first
+    trial = get_news("Starbucks", "2023-11-15", 1, api_token, conn) # initialize the class object
+    trial_data = trial.retrieve_raw_data() 
+    news_number = int(trial_data[0]["meta"]['found']) # number of news articles
+    
+    # Each page contains 25 news articles, we want to calculate the total pages we have
+    # so that we can loop through the number of pages.
+    pages = news_number // 25 
+    if news_number % 25 != 0:
+        pages += 1
+    
+    # now we know the number of pages, we call another class object with this number
+    starbucks = get_news("Starbucks", "2023-11-15", pages, api_token, conn)
+    starbucks_data = starbucks.retrieve_raw_data()
+    
+    # create df columns, each column contains values of corresponding feature of news articles
+    # for example, the column named title contains titles of news articles. 
+    for x in elements:
+        ele = starbucks.element(starbucks_data, x)
+        df[x] = ele
+    
+    clean_df = starbucks.clean(df) # clean the dataframe
+    clean_df.to_csv("Starbucks_news.csv") # export to this .csv file.
+
+#***************************************************************************#
+    
+# we've already retrieved and saved the news article; it's recommended to read the .csv file directly:
+    df = pd.read_csv("Starbucks_news.csv")
+    starbucks = get_news()
+    starbucks.understand_data(df)
+    
---
+++ ourProject/.ipynb_checkpoints/stockprice-checkpoint.py
@@ -0,0 +1,331 @@
+import pandas as pd
+import numpy as np
+from yahoo_fin import stock_info as si
+from matplotlib import pyplot as plt
+from plotly import express as px
+import plotly.subplots as sp
+import plotly.graph_objects as go
+
+from sklearn.model_selection import train_test_split # splitting data
+from sklearn.metrics import mean_absolute_error, mean_squared_error  # assessing data
+from tensorflow.keras.models import Sequential # type of model
+from tensorflow.keras.layers import LSTM, Dense, Dropout # layers
+from tensorflow.keras.callbacks import EarlyStopping # model modifications
+from sklearn.preprocessing import MinMaxScaler # data manipulation
+
+
+        
+def create_data(code = "SBUX", start = "2021-01-04", end = "2023-11-14"):
+    '''
+    This function creates the stock price dataset with specified code, start, and end.
+    We use a package called yahoo_fin to retrieve data. 
+    
+    @param code: str, ticker symbol of company, default is sbux;
+    @param start: str, start date of the stock price, default is 2021-01-03;
+    @param end: str, end date of the stock price, default is 2023-11-14.
+    
+    @rvalue: dataframe, the return value is the pandas dataframe that contains the desired stock price. 
+    '''
+    
+    company = si.get_data(code)
+    company = company.loc[start:end] 
+    company["date"] = company.index # create a data column
+    company = company.reset_index() # reset_index so that index starts from 0
+    company.drop(columns = "index", inplace = True)
+        
+    return company
+    
+def stock_price_change(data):
+    '''
+    This function creates a plot that visualizes the daily stock changes in real terms. Close price of Starbucks
+    is considered, but not open price, as the former is more representative.
+    
+    @param data: dataframe, the dataset we get from method create_data()
+        
+    The method has no return values, but plot a figure to present daily stock changes.
+    '''
+
+    fig = px.line(data, 
+                  x = "date",
+                  y = "close",
+                  hover_data = ["close"],
+                  title = "Stock price change (real term)")
+
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.update_layout(margin=dict(l=20, r=20, t=30, b=20))
+    fig.show()
+        
+def stock_price_percentage(data):
+    '''
+    This function creates a plot that visualizes the daily stock changes in percentage terms. 
+    Again, close price of Starbucks is considered.
+        
+    @param data: dataframe, the dataset we get from method create_data()
+        
+    The method has no return values, but plot a figure to present daily percentage stock changes.
+    '''
+        
+    # percentage change satisfies (today - yesterday) / yesterday
+    data["yesterday"] = data["close"].shift(1) # shift the column downward by 1 row
+    data = data.fillna(data["close"][0]) # fill nan with the first entry of close
+    data["%change"] = (data["close"]- data["yesterday"]) / data["yesterday"]
+    avg = np.mean(data["%change"])
+
+    fig = px.line(data, 
+                  x = "date",
+                  y = "%change",
+                  hover_data = ["close", "%change"],
+                  title = "Stock price change (% change)")
+
+    fig.add_hline(y = avg,
+                  line_color = "red",
+                  annotation_text = "{}%".format(round(avg*100, 3)),
+                  annotation_position="bottom left",
+                  annotation_font_size=25,
+                  annotation_font_color="black")
+
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.update_layout(margin=dict(l=20, r=20, t=30, b=20))
+    fig.show()
+
+def clean_data(data):
+    '''
+    This function drops any NA values from the data.
+
+    @param data: dataframe with stock price data
+
+    The function returns data without NA values.
+    '''
+    return data.dropna(ignore_index = True)
+
+def add_features(data):
+    '''
+    This function adds predictor variables to the stock price dataset. The "quarterly_reports" column identifies months when quarterly
+    earnings for a public company are released. It adds columns for the difference of the open and closing stock prices, and the low and high
+    stock prices. It also adds a column for the estimaged moving average with data from 100 days. Lastly it adds two columns for the Bollinger
+    Bands using the moving average and moving average standard deviation to create bounds for the stock prices. The function also sets the 
+    date as the index and removes NA values.
+
+    @param data: dataframe with stock price data
+
+    The function returns data with new columns and without NA values.
+    '''
+    # add column for months when quarter earnings reports are released: 
+    split_date = data["date"].astype(str).str.split("-", expand = True) # year, month, day
+    data["month"] = split_date[1].astype("int")
+    data["day"] = split_date[2].astype("int")
+    data["year"] = split_date[0].astype("int")
+    data['date'] = pd.to_datetime(data["date"])
+    data["quarterly_reports"] = data['date'].dt.month.isin([1, 4, 7, 10]).astype(int)
+    
+    # add open-close and low-high columns
+    data["open-close"] = data["open"] - data["close"]
+    data["low-high"] = data["low"] - data["high"]
+    
+    # add Estimated Moving Average using time window of 100 days
+    data['ema100'] = data['close'].ewm(span=100, adjust=False).mean()
+    
+    # add Bollinger Bands
+    data['moving_avg20'] = data['close'].rolling(window=20).mean() # moving average for 20 day period
+    data['moving_avg20std'] = data['close'].rolling(window=20).std() # moving average standard deviation for 20 day period
+    data['bollinger_upper'] = data['moving_avg20'] + (data['moving_avg20std'] * 2)
+    data['bollinger_lower'] = data['moving_avg20'] - (data['moving_avg20std'] * 2)
+    
+    # set date as index
+    data.set_index('date', inplace=True)
+    
+    # replace NA in "close" column with last valid closing price
+    na_index = data.at[data.index.max(), 'close']    
+    data.fillna((na_index), inplace=True)
+    
+    return data
+
+def pred_target(features, data):
+    '''
+    This function creates two datasets (predictor and target datasets) for the model. 
+
+    @param features: list of column names in dataset that will be used as the predictor data
+    @param data: stock price data
+
+    The function returns the dataset filtered by the features and a dataset with just closing prices.
+    '''
+    return data[features], data["close"]
+
+def scalers(pred, target):
+    '''
+    This function defines the MinMaxScaler for the predictor and target data, as well as scales the predictor and target data to values from 
+    0 to 1.
+
+    @param pred: predictor data
+    @param target: target data
+
+    The function returns two scalers and the scaled predictor and target data.
+    '''
+    # normalize between 0 and 1
+    # define scalers
+    scaler_pred = MinMaxScaler() # for scaling multiple columns
+    scaler_target = MinMaxScaler() # for scaling one column
+    # normalize data
+    scaled_pred = scaler_pred.fit_transform(pred.values)
+    scaled_target = scaler_target.fit_transform(target.values.reshape(-1,1))
+    return scaler_pred, scaler_target, scaled_pred, scaled_target
+
+def train_val_test(pred, seq_len, scaled_p):
+    '''
+    This function defines the training, validation, and testing data. 
+
+    @param pred: predictor data
+    @param seq_len (int): represents the number of time steps model will use to predict stock price
+    @param scaled_p: scaled predictor data
+
+    The function returns the training, validation, and test data.
+    '''
+    train_len = int(0.7*len(pred)) # define training length
+    val_len = int(0.2*train_len) # validations set is 20% of training set
+    
+    val_data = scaled_p[:val_len,:] # subset validation data
+    train_data = scaled_p[val_len:train_len, :] # subset training data
+    test_data = scaled_p[train_len - seq_len:, :] # subset test data with seq_len
+    
+    return train_data, val_data, test_data
+
+def split_data(seq_len, data, index_close):
+    '''
+    This function splits time series data into input sequences and corresponding output values for single-step prediction.
+
+    @param seq_len (int): represents the number of time steps model will use to predict stock price
+    @param data: 2D array containing time series data where rows represent time steps and columns represent features
+    @index_close (int): index of the column containing the target variable (closing prices)
+
+    The function returns:
+    x: input sequences each of shape (seq_len, num_features)
+    y: corresponding output values, representing the target variable for single-step prediction
+    '''
+    x, y = [], []
+    data_len = data.shape[0]
+
+    for i in range(seq_len, data_len):
+        x.append(data[i-seq_len:i,:]) # previous 50 day values
+        y.append(data[i, index_close]) #contains the prediction values for validation, for single-step prediction
+    
+    # Convert the x and y to numpy arrays
+    x = np.array(x)
+    y = np.array(y)
+    return x, y
+
+def compile_train(model, x_train, y_train, x_val, y_val):
+    '''
+    This function takes in a model and compiles it using the mean squared error as the loss function. It then trains the model on the training
+    and validation data using callbacks.
+
+    @param model: Sequential model to predict stock price
+    @param x_train: training input sequence data
+    @param y_train: training target data
+    @param x_val: validation input sequence data
+    @param y_val: validation target data
+
+    The function returns the history and metrics of the model as it is being trained. 
+    '''
+    # compile the model 
+    model.compile(optimizer="adam", loss="mean_squared_error")
+
+    # train Model
+    callback = EarlyStopping(monitor='loss', patience=5, verbose=1)
+    return model.fit(x_train, y_train, 
+                    batch_size = 16, 
+                    epochs = 50,
+                    callbacks = [callback],
+                    validation_data=(x_val, y_val)
+                   )
+
+def plot_history(history):
+    '''
+    This function plots the model loss for each epoch of the model's performance on the training and validation data.
+
+    @param history: History object of a model
+
+    The function does not return anything and instead plots the model loss. 
+    '''
+    # create subplot 
+    fig = sp.make_subplots(rows=1, cols=1, subplot_titles=["Model loss"])
+
+    # plot training loss
+    fig.add_trace(go.Scatter(x=list(range(1, len(history.history["loss"]) + 1)), y=history.history["loss"], mode='lines', name='Train'))
+
+    if 'val_loss' in history.history:
+        # plot validation loss if available
+        fig.add_trace(go.Scatter(x=list(range(1, len(history.history["val_loss"]) + 1)), y=history.history["val_loss"], 
+                                 mode='lines', name='Validation'))
+
+    # set layout options
+    fig.update_layout(
+        xaxis=dict(title="Epoch"),
+        yaxis=dict(title="Loss"),
+        legend=dict(x=1.02, y=1, traceorder='normal', orientation='v'),
+        width=800,  # adjust the width as needed
+        height=400  # adjust the height as needed
+    )
+
+    # show figure
+    fig.show()
+    
+def evaluate_model(model, x_test, y_test, scaler_target):
+    '''
+    This function evaluates the performance of the model using the test data. It plots the performance of the model on the test data and plots
+    the actual closing values. It also calculates the mean absolute error, mean absolute percentage error, and median absolute percentage
+    error for the actual and predicted closing values.
+
+    @param model: Sequential model predicting stock price
+    @param x_test: testing input sequence data
+    @param y_test: testing target data
+    @param scaler_target: MinMaxScaler for the target data
+
+    The function does not return anything and instead plots the predicted and actual closing stock prices and prints out the three metrics. 
+    '''
+    y_predicted_close = model.predict(x_test) # predict closing prices
+    y_predicted_close = scaler_target.inverse_transform(y_predicted_close) # unscale predictions
+    y_true_close= scaler_target.inverse_transform(y_test.reshape(-1, 1))
+
+    # Create traces for actual and predicted closing prices
+    trace_actual = go.Scatter(x=np.arange(len(y_true_close)), y=y_true_close.flatten(),
+                          mode='lines', name='Actual Closing Prices', line=dict(color='blue'))
+
+    trace_predicted = go.Scatter(x=np.arange(len(y_predicted_close)), y=y_predicted_close.flatten(),
+                             mode='lines', name='Predicted Closing Prices', line=dict(color='red'))
+
+    # Create layout
+    layout = go.Layout(title='Actual vs Predicted Closing Prices',
+                   xaxis=dict(title='Days'),
+                   yaxis=dict(title='Closing Prices'),
+                   showlegend=True,
+                   legend=dict(x=1, y=1, traceorder='normal', orientation='v'))
+
+    # Create figure
+    fig = go.Figure(data=[trace_actual, trace_predicted], layout=layout)
+
+    # Show the figure
+    fig.show()
+
+    # Mean Absolute Error (MAE)
+    MAE = mean_absolute_error(y_true_close, y_predicted_close)
+    print(f'Mean Absolute Error (MAE): {np.round(MAE, 2)}')
+
+    # Mean Absolute Percentage Error (MAPE)
+    MAPE = np.mean((np.abs(np.subtract(y_true_close, y_predicted_close) / y_true_close))) * 100
+    print(f'Mean Absolute Percentage Error (MAPE): {np.round(MAPE, 2)} %')
+
+    # Median Absolute Percentage Error (MDAPE)
+    MDAPE = np.median((np.abs(np.subtract(y_true_close, y_predicted_close) / y_true_close))) * 100
+    print(f'Median Absolute Percentage Error (MDAPE): {np.round(MDAPE, 2)} %')
+
+
+
+
+
+
+
+
+
+
